062b94816b0387ec70063be50c4adeb777727d2e
Chizobaonorh <onorhreward@gmail.com>
2025-09-27 02:56:13 +0100

    new changes

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from google.colab import drive
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import IsolationForest
+
+data = pd.read_csv("./dataset/transactions.csv")
+
+type = data["type"].value_counts()
+transactions = type.index
+quantity = type.values
+
+figure = px.pie(
+                values=quantity,
+                names=transactions,
+                title="Distribution of Transaction Type"
+                )
+figure.show()
+
+data['type'] = data['type'].map({"CASH_OUT": 1, 
+                                "PAYMENT": 2, 
+                                "CASH_IN": 3, 
+                                "TRANSFER": 4, 
+                                "DEBIT": 5})
+data["isFraud"] = data["isFraud"].map({0: "No Fraud", 1: "Fraud"})
+
+x = np.array(data[['type', 'amount', 'oldbalanceOrg', 'newbalanceOrig']])
+y = np.array(data['isFraud'])
+
+model = DecisionTreeClassifier()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test))
+
+features = np.array([[1, 2806.0,2806.0,0.0]])
+print(model.predict(features))
